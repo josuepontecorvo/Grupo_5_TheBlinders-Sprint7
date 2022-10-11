a1c27855ca7f9daf26b6987252b52917cbe347ce
Josue Pontecorvo <josuepontecorvo@gmail.com>
2022-10-11 19:10:51 -0300

    Test Heroku

--- src/dataBase/config/config.js
+++ src/dataBase/config/config.js
@@ -8,6 +8,13 @@ module.exports = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: process.env.DB_DIALECT
-  }
-
+  },
+  "production": {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT
+    }
 }
