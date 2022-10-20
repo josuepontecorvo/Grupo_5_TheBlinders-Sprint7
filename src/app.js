58d25fb184710dac221f42ec5d4619cea0ce7f60
Josue Pontecorvo <josuepontecorvo@gmail.com>
2022-10-20 13:51:58 -0300

    api/usuarios/ingresar added

--- src/app.js
+++ src/app.js
@@ -8,12 +8,14 @@ const userLoggedMiddleware = require('./middleware/userLoggedMiddleware');
 const mainRouter = require('./routes/mainRouter');
 const usersRouter = require('./routes/usersRouter');
 const productsRouter = require('./routes/productsRouter');
+const cors = require('cors');
 // API routers
 const APIUsersRouter = require('./routes/API/usersRouter');
 const APIProductsRouter = require('./routes/API/productsRouter');
 
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.static(path.resolve(__dirname, '../public')));
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, 'views'));
