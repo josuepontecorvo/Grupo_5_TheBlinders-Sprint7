a129b19c2207f1e54dfde532411f3ae06aa2e3f1
Josue Pontecorvo <josuepontecorvo@gmail.com>
2022-10-25 12:43:00 -0300

    window.onload deleted from searchBar.js

--- public/js/searchBar.js
+++ public/js/searchBar.js
@@ -1,10 +1,8 @@
-window.onload = function () {
-    const searchInput = document.querySelector(".search-input");
-    const form = document.querySelector(".search-bar")
+const searchInput = document.querySelector(".search-input");
+const form = document.querySelector(".search-bar")
 
-    form.addEventListener("submit", (e) => {
-        if (searchInput.value.trim() == "") {
-            e.preventDefault();
-        }
-    })
-}
+form.addEventListener("submit", (e) => {
+    if (searchInput.value.trim() == "") {
+        e.preventDefault();
+    }
+})
